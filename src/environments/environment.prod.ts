1d8bd469b89217d6023a8b10411b67951addd99c
Timon Grassl <grassl.timon@gmail.com>
2020-02-29 10:42:57 +0100

    revert fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: '/api',
+  apiUrl: 'https://ap-2020-p03.herokuapp.com',
   tinyApiKey: '74bsmko53jk0xthi48bvg2lqe2ntdkqgvivrc7xlijcjk51k',
 };
