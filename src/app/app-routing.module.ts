076838fd6a2e5c9c462f3174d1c0a788f1ba067b
Timon Grassl <grassl.timon@gmail.com>
2020-03-13 11:31:23 +0100

    restore scroll position on route change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)},
-  {path: 'map', loadChildren: () => import('./pages/map/map.module').then(m => m.MapModule)},
-  {path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule), canActivate: [AuthGuard]},
-  {path: 'register', loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterModule), canActivate: [AuthGuard]},
-  {path: 'notes', loadChildren: () => import('./pages/notes/notes.module').then(m => m.NotesModule), canActivate: [AuthGuard]},
-  {path: '**', redirectTo: ''},
+  { path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'map', loadChildren: () => import('./pages/map/map.module').then(m => m.MapModule) },
+  { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule), canActivate: [AuthGuard] },
+  {
+    path: 'register', loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterModule),
+    canActivate: [AuthGuard]
+  },
+  { path: 'notes', loadChildren: () => import('./pages/notes/notes.module').then(m => m.NotesModule), canActivate: [AuthGuard] },
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {onSameUrlNavigation: 'reload'})],
+  imports: [RouterModule.forRoot(routes,
+    {
+      onSameUrlNavigation: 'reload',
+      scrollPositionRestoration: 'enabled',
+    })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
